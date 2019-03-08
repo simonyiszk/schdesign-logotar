14ec751b61664c0c16dd68c23d8aacae98d1a429
patriktacs <acs.patrik@simonyi.bme.hu>
2019-03-08 22:29:31 +0100

    responsive menu button error correction

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,18 +1,22 @@
 let hamburger = document.getElementsByClassName("hamburger-button");
 let hiddenMenu = document.getElementsByClassName("hidden-menu");
-var isHiddenMenuActive = false
+let hamburgerButton = hamburger[0].getElementsByClassName("fa-bars");
+var isHiddenMenuActive = false;
+
 hamburger[0].onclick = function(){
 	if (!isHiddenMenuActive){
-		isHiddenMenuActive = true
+		isHiddenMenuActive = true;
 		hiddenMenu[0].style.visibility = "visible";
 		hiddenMenu[0].style.zIndex = "2";
+		hamburgerButton[0].classList.add("fa-times");
 	}
 	else if(isHiddenMenuActive){
-		isHiddenMenuActive = false
+		isHiddenMenuActive = false;
 		hiddenMenu[0].style.visibility = "hidden";
 		hiddenMenu[0].style.zIndex = "-1";
+		hamburgerButton[0].classList.remove("fa-times");
 	}
 	else{
-		console.log("Error #1: isHiddenMenuActive invalid data");
+		console.log("Error #1: isHiddenMenuActive has invalid data");
 	}
 }
