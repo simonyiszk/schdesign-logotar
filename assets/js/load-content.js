7e4184b884a1f5ae693e4935dcfa2327b0dd3752
patriktacs <acs.patrik@simonyi.bme.hu>
2019-03-09 22:22:35 +0100

    working grid build :)

--- assets/js/load-content.js
+++ assets/js/load-content.js
@@ -8,7 +8,14 @@ window.onload = initGridControl
 
 function initGridControl(){
 	menuItems = document.getElementsByClassName("menu-items")[0];
+	hiddenMenuItems = document.getElementsByClassName("hidden-menu")[0];
 	menuItems.addEventListener("click", function(){
+		let targetElement = event.target
+		if (targetElement.tagName == "A"){
+			buildGrid(event.target.dataset.group);
+		}
+	});
+	hiddenMenuItems.addEventListener("click", function(){
 		let targetElement = event.target
 		if (targetElement.tagName == "A"){
 			buildGrid(event.target.dataset.group);
@@ -17,7 +24,6 @@ function initGridControl(){
 }
 
 function buildGrid(groupNameFromCaller) {
-	console.log(groupNameFromCaller);
 
 	cardGrid = document.getElementsByClassName("logo-library")[0];
 	document.body.removeChild(cardGrid);
@@ -30,44 +36,45 @@ function buildGrid(groupNameFromCaller) {
 
 	request.onload = function () {
   		dataBase = JSON.parse(this.response);
-  		console.log(dataBase);
   		for (i = 0; i < dataBase.length; i++){
-  			let tempASvg = document.createElement("a");
-  			tempASvg.innerHTML = "SVG";
-  			tempASvg.href= "assets/database/" + dataBase[i].parentGroup + "/" + dataBase[i].groupName + "/" + dataBase[i].groupName + ".svg";
-  			tempASvg.download = dataBase[i].groupName + ".svg"
-
-  			let tempAPng = document.createElement("a");
-  			tempAPng.innerHTML = "PNG";
-  			tempAPng.href= "assets/database/" + dataBase[i].parentGroup + "/" + dataBase[i].groupName + "/" + dataBase[i].groupName + ".png";
-  			tempAPng.download = dataBase[i].groupName + ".png"
-
-  			let tempAAi = document.createElement("a");
-  			tempAAi.innerHTML = "AI";
-  			tempAAi.href= "assets/database/" + dataBase[i].parentGroup + "/" + dataBase[i].groupName + "/" + dataBase[i].groupName + ".ai";
-  			tempAAi.download = dataBase[i].groupName + ".ai"
-
-  			let tempBotSpan = document.createElement("span");
-  			tempBotSpan.appendChild(tempASvg);
-  			tempBotSpan.appendChild(tempAPng);
-  			tempBotSpan.appendChild(tempAAi);
-
-  			let groupFullName = document.createElement("p");
-  			groupFullName.innerHTML = dataBase[i].groupFullName;
-
-  			let tempSecondLayerSpan = document.createElement("span");
-  			tempSecondLayerSpan.appendChild(groupFullName); 
-  			tempSecondLayerSpan.appendChild(tempBotSpan);
-
-  			let groupLogoImage = document.createElement("img");
-  			groupLogoImage.src = "assets/database/" + dataBase[i].parentGroup + "/" + dataBase[i].groupName + "/display-image.png";
-  			groupLogoImage.alt = dataBase[i].groupFullName;
-
-  			let tempCard = document.createElement("span");
-  			tempCard.appendChild(groupLogoImage);
-  			tempCard.appendChild(tempSecondLayerSpan);
-
-  			cardGrid.appendChild(tempCard);
+  			if(groupNaemFromCaller == dataBase[i]..parentGroup){
+  				let tempASvg = document.createElement("a");
+  				tempASvg.innerHTML = "SVG";
+  				tempASvg.href= "assets/database/" + dataBase[i].parentGroup + "/" + dataBase[i].groupName + "/" + dataBase[i].groupName + ".svg";
+	  			tempASvg.download = dataBase[i].groupName + ".svg"
+	
+	  			let tempAPng = document.createElement("a");
+	  			tempAPng.innerHTML = "PNG";
+	  			tempAPng.href= "assets/database/" + dataBase[i].parentGroup + "/" + dataBase[i].groupName + "/" + dataBase[i].groupName + ".png";
+	 			tempAPng.download = dataBase[i].groupName + ".png"
+	
+	  			let tempAAi = document.createElement("a");
+	  			tempAAi.innerHTML = "AI";
+	  			tempAAi.href= "assets/database/" + dataBase[i].parentGroup + "/" + dataBase[i].groupName + "/" + dataBase[i].groupName + ".ai";
+	  			tempAAi.download = dataBase[i].groupName + ".ai"
+	
+	  			let tempBotSpan = document.createElement("span");
+	  			tempBotSpan.appendChild(tempASvg);
+	  			tempBotSpan.appendChild(tempAPng);
+	  			tempBotSpan.appendChild(tempAAi);
+	
+	  			let groupFullName = document.createElement("p");
+	  			groupFullName.innerHTML = dataBase[i].groupFullName;
+	
+	  			let tempSecondLayerSpan = document.createElement("span");
+	  			tempSecondLayerSpan.appendChild(groupFullName); 
+	  			tempSecondLayerSpan.appendChild(tempBotSpan);
+	
+	  			let groupLogoImage = document.createElement("img");
+	  			groupLogoImage.src = "assets/database/" + dataBase[i].parentGroup + "/" + dataBase[i].groupName + "/display-image.png";
+	  			groupLogoImage.alt = dataBase[i].groupFullName;
+	
+	  			let tempCard = document.createElement("span");
+	  			tempCard.appendChild(groupLogoImage);
+	  			tempCard.appendChild(tempSecondLayerSpan);
+	
+	  			cardGrid.appendChild(tempCard);
+  			}
   		}
 	}
 
