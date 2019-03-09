88a8b0847c020f838b2bb8f9f2ff21c9323af71b
patriktacs <acs.patrik@simonyi.bme.hu>
2019-03-09 21:12:14 +0100

    changed fetch to request

--- assets/js/load-content.js
+++ assets/js/load-content.js
@@ -2,15 +2,13 @@ var dataBase;
 
 
 window.onload = function(){
-	fetch('https://schdesign-logotar.netlify.com/database.json').then(response => {
-  return response.json();
-}).then(data => {
-  dataBase = JSON.parse(data);
-  console.log(dataBase.lenght());
-  var tempDisplayImage = document.createElement("img");
-  tempDisplayImage.src = "./assets/database/" + dataBase[0].parentGroup + "/" + database[0].name + "/display-image.png";
-}).catch(err => {
-  // What do when the request fails
-  console.log('The request failed!'); 
-});
+	let request = new XMLHttpRequest();
+	request.open('GET', './database.json', true);
+
+	request.onload = function () {
+  		dataBase = JSON.parse(this.response);
+  		console.log(dataBase.length);
+	}
+
+	request.send();
 }
