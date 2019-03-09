948d0db6bf5bfb1eefa71dea83707a2cdac28ecd
patriktacs <acs.patrik@simonyi.bme.hu>
2019-03-09 20:54:23 +0100

    created database structure

--- assets/js/load-content.js
+++ assets/js/load-content.js
@@ -2,11 +2,13 @@ var dataBase;
 
 
 window.onload = function(){
-	fetch('database.json').then(response => {
+	fetch('./assets/js/database.json').then(response => {
   return response.json();
 }).then(data => {
   dataBase = JSON.parse(data);
   console.log(dataBase.lenght());
+  var tempDisplayImage = document.createElement("img");
+  tempDisplayImage.src = "./assets/database/" + dataBase[0].parentGroup + "/" + database[0].name + "/display-image.png";
 }).catch(err => {
   // What do when the request fails
   console.log('The request failed!'); 
