7e7c2bc69b4583528a4b9452cadad2333e8ac6d6
patriktacs <acs.patrik@simonyi.bme.hu>
2019-03-09 13:01:16 +0100

    menu script code quality improve

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,25 +1,41 @@
-window.onload = function(){
-	let hamburger = document.getElementsByClassName("hamburger-button")[0];
-	let hiddenMenu = document.getElementsByClassName("hidden-menu")[0];
-	let hamburgerButton = hamburger.getElementsByClassName("fa-bars")[0];
-	var isHiddenMenuActive = false;
+var hamburger;
+var hiddenMenu;
+var hamburgerButton;
+var isHiddenMenuActive = false;
 
-	hamburger.onclick = function(){
-		if (!isHiddenMenuActive){
-			isHiddenMenuActive = true;
-			hiddenMenu.style.visibility = "visible";
-			hiddenMenu.style.zIndex = "2";
-			hamburgerButton.classList.toggle("fa-times");
+window.onload = initMenu
+
+function initMenu() {
+	hamburger = document.getElementsByClassName("hamburger-button")[0];
+	hiddenMenu = document.getElementsByClassName("hidden-menu")[0];
+	hamburgerButton = hamburger.getElementsByClassName("fa-bars")[0];
+
+	hamburger.onclick = toggleHamburger;
+
+	hiddenMenu.addEventListener("click", function(){
+		let targetElement = event.target
+		if (targetElement.tagName == "A"){
+			toggleHamburger();
 		}
-		else if(isHiddenMenuActive){
-			isHiddenMenuActive = false;
-			hiddenMenu.style.visibility = "hidden";
-			hiddenMenu.style.zIndex = "-1";
-			hamburgerButton.classList.toggle("fa-times");
-		}
-		else{
-			console.log("Error #1: isHiddenMenuActive has invalid data");
-		}
+	});
+}
+
+function toggleHamburger(){
+	if (!isHiddenMenuActive){
+		isHiddenMenuActive = true;
+		hiddenMenu.style.visibility = "visible";
+		hiddenMenu.style.zIndex = "2";
+		hamburgerButton.classList.toggle("fa-times");
+	}
+	else if(isHiddenMenuActive){
+		isHiddenMenuActive = false;
+		hiddenMenu.style.visibility = "hidden";
+		hiddenMenu.style.zIndex = "-1";
+		hamburgerButton.classList.toggle("fa-times");
+	}
+	else{
+		console.log("Error #1: isHiddenMenuActive has invalid data");
 	}
 }
+
 
