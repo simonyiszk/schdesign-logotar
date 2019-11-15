129eba285c17eb4b84757548a21a429a4456cd08
AndrisBorbas <andris.borbas@gmail.com>
2019-11-15 13:49:25 +0100

    add cookies to dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,29 @@ const buildMenu = () => {
   menu.addEventListeners();
 };
 
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 const init = async() => {
   await db.update();
   buildMenu();
@@ -29,30 +52,37 @@ init();
 
 document.addEventListener('DOMContentLoaded', function() {
   var checkbox = document.querySelector('input[type="checkbox"]');
+  var isDarkMode = getCookie('isDarkMode');
+  if (isDarkMode) checkbox.checked = true;
+  else checkbox.checked = false;
 
   if (checkbox.checked) {
     let navBar = document.getElementsByTagName('body')[0];
     let logoLibrary = document.getElementsByClassName('logo-library')[0];
-    navBar.classList.toggle("dark");
-    logoLibrary.classList.toggle("dark");
+    navBar.classList.toggle("dark", true);
+    logoLibrary.classList.toggle("dark", true);
+    setCookie('isDarkMode', true, 7);
   } else {
     let navBar = document.getElementsByTagName('body')[0];
     let logoLibrary = document.getElementsByClassName('logo-library')[0];
-    navBar.classList.toggle("dark");
-    logoLibrary.classList.toggle("dark");
+    navBar.classList.toggle("dark", false);
+    logoLibrary.classList.toggle("dark", false);
+    setCookie('isDarkMode', false, 7);
   }
 
   checkbox.addEventListener('change', function() {
     if (checkbox.checked) {
       let navBar = document.getElementsByTagName('body')[0];
       let logoLibrary = document.getElementsByClassName('logo-library')[0];
-      navBar.classList.toggle("dark");
-      logoLibrary.classList.toggle("dark");
+      navBar.classList.toggle("dark", true);
+      logoLibrary.classList.toggle("dark", true);
+      setCookie('isDarkMode', true, 7);
     } else {
       let navBar = document.getElementsByTagName('body')[0];
       let logoLibrary = document.getElementsByClassName('logo-library')[0];
-      navBar.classList.toggle("dark");
-      logoLibrary.classList.toggle("dark");
+      navBar.classList.toggle("dark", false);
+      logoLibrary.classList.toggle("dark", false);
+      setCookie('isDarkMode', false, 7);
     }
   });
 });
