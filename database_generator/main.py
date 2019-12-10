6ca7fac927160393a05e9cfd12a353762a7c1cf9
AndrisBorbas <andris.borbas@gmail.com>
2019-12-10 01:56:21 +0100

    Update database generator, now skips Temp folders
    That very pretty dictionary generator was so hard to reverse engineer, to add conditions, mah gad

--- database_generator/main.py
+++ database_generator/main.py
@@ -15,25 +15,27 @@ root = Path(os.path.join(os.path.realpath(os.curdir), config["database_root"]))
 
 allfiles = ["asd"]
 
+
 def check_file(fname):
     for ef in allfiles:
         if fname == ef:
             return True
     return False
 
+
 def get_file(path, extension):
-    if extension=="preview":
-        extension="png"
+    if extension == "preview":
+        extension = "png"
     for file in path.files:
         if file.name.endswith(extension):
-            #print(file.real_path)
+            # print(file.real_path)
             if check_file(file.real_path):
-                #print(allfiles)
+                # print(allfiles)
                 continue
             else:
                 allfiles.append(file.real_path)
                 asd = file.webify(root.real_path, config["database_root_url"])
-                #print(asd)
+                # print(asd)
                 return asd
 
     #print(f"Can't find file in {path.real_path} with extension .{extension}.")
@@ -51,6 +53,11 @@ def object2json2file(obj, file_name):
 
 if __name__ == '__main__':
 
+    """
+    Mire rájöttem hogy ez wtf, holy moly
+    """
+
+    """
     obj = {
         reszort.name: {
             kör.name: {
@@ -58,10 +65,29 @@ if __name__ == '__main__':
             } for kör in reszort.directories
         } for reszort in root.directories
     }
-    
+    """
+
+    obj = {}
+
+    for reszort in root.directories:
+
+        if reszort.name != "Temp" and reszort.name != "Ref":
+            asd1 = {}
+            for kör in reszort.directories:
+                asd2 = {}
+                for ext in config["extensions"]:
+                    fájl = get_file(kör, ext)
+                    asd2[ext] = fájl
+                asd1[kör.name] = asd2
+            obj[reszort.name] = asd1
+
     for k1 in list(obj):
+        # print(k1)
+        # print(obj[k1])
         for k2 in list(obj[k1]):
+            # print(k2)
             for k3 in list(obj[k1][k2]):
+                # print(obj[k1][k2][k3])
                 """
                 # Check for preview files
                 if k3 == "preview":
@@ -71,9 +97,9 @@ if __name__ == '__main__':
                 """
                 # Remove not found files
                 if obj[k1][k2][k3] == None:
-                    #print(obj[k1][k2][k3])
+                    # print(obj[k1][k2][k3])
                     del obj[k1][k2][k3]
-    
-    #print(obj)
+
+    # print(obj)
 
     object2json2file(obj, config["target_json"])
