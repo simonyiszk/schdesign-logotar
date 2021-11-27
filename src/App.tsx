438ec70e29fc7045decef5b0e82158536554d2bc
Gergő Radeczki <g.radeczki@gmail.com>
2021-11-27 20:06:54 +0100

    React with Material UI and auto theme detection

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import {createTheme, CssBaseline, ThemeProvider, useMediaQuery} from "@mui/material";
+
+function App() {
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode: prefersDarkMode ? 'dark' : 'light',
+                },
+            }),
+        [prefersDarkMode],
+    );
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <p>Logótár</p>
+        </ThemeProvider>
+    );
+}
+
+export default App;
