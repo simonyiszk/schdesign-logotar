1aab2651857cb6d36e404186a8c16dc170447f43
Gergő Radeczki <g.radeczki@gmail.com>
2021-12-18 17:20:21 +0100

    unused stuff removed

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,18 +1,12 @@
 import {
-    Box,
     Button,
     Card,
     CardActions,
     CardContent,
     CardMedia,
-    FormControl, IconButton,
-    InputLabel, MenuItem,
-    Select, SelectChangeEvent,
     Typography
 } from "@mui/material";
 import {ILink} from "../../data/data";
-import {useState} from "react";
-import DownloadIcon from '@mui/icons-material/Download';
 
 interface LogoProps {
     name: string
@@ -23,23 +17,14 @@ interface LogoProps {
 
 function Logo(props: LogoProps) {
 
-    let {name, preview, links} = props
-
-    const [format, setFormat] = useState(links[0].name);
-
-
-    preview = "https://dummyimage.com/280x210/999/fff"
-
-    const handleChange = (event: SelectChangeEvent) => {
-        setFormat(event.target.value as string);
-    };
+    const {name, preview, links} = props
 
     return (
         <Card sx={{ maxWidth: 280 }}>
             <CardMedia
                 component="img"
                 height="210"
-                image={preview}
+                image={preview === "" ? "https://dummyimage.com/280x210/999/fff" : preview}
                 alt={name}
             />
             <CardContent>
