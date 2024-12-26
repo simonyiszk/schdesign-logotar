58a084c13fff8ab75635ff50957a978428040391
Miklos Daniel <daniel.miklos0@gmail.com>
2024-12-26 01:11:52 +0100

    icon with param

--- src/lib/components/icons/Icon.svelte
+++ src/lib/components/icons/Icon.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
   import LogoMini from "../schdesign/LogoMini.svelte"
   import * as Tooltip from "$lib/components/ui/tooltip"
+
+  interface Props {
+    name: string
+    path: string
+  }
+
+  const { path, name }: Props = $props()
 </script>
 
 <Tooltip.Provider>
@@ -9,11 +16,12 @@
       <div
         class="aspect-square size-full rounded-xl bg-muted p-4 transition hover:bg-muted-foreground/20"
       >
-        <LogoMini />
+        <!-- svelte-ignore a11y_missing_attribute -->
+        <img class="size-full object-contain" src={path} />
       </div>
     </Tooltip.Trigger>
     <Tooltip.Content side="bottom">
-      <p>asdasdasd</p>
+      <p>{name}</p>
     </Tooltip.Content>
   </Tooltip.Root>
 </Tooltip.Provider>
