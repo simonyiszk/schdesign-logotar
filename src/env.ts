1b58d001e3f6c945d9b3eaf40816fe30d809931e
Gergő Radeczki <g.radeczki@gmail.com>
2025-03-24 02:34:44 +0100

    Require new env variable

--- src/env.ts
+++ src/env.ts
@@ -5,10 +5,12 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
     PAYLOAD_SECRET: z.string().min(1),
+    BLOB_READ_WRITE_TOKEN: z.string().min(1),
   },
   client: {},
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     PAYLOAD_SECRET: process.env.PAYLOAD_SECRET,
+    BLOB_READ_WRITE_TOKEN: process.env.BLOB_READ_WRITE_TOKEN
   },
 });
