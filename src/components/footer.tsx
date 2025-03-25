16c9c92e8094f790b5af6e349e85a920df07967b
Gergő Radeczki <g.radeczki@gmail.com>
2025-03-25 17:00:43 +0100

    Add better footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,16 +1,221 @@
+import GitHubIcon from "@mui/icons-material/GitHub";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import EmailIcon from "@mui/icons-material/Email";
+import { Button, Container, Grid2, Typography } from "@mui/material";
+import Image from "next/image";
+
 export function Footer() {
   return (
     <footer
       sx={{
-        width: "100%",
         display: "flex",
-        flexDirection: "row",
+        flexDirection: "column",
+        width: "100%",
+        gap: "3rem",
         padding: "1rem",
-        justifyContent: "center",
-        backgroundColor: "lightgray",
+        paddingTop: "3rem",
+        backgroundColor: "#2a2a2a",
       }}
     >
-      <p sx={{ color: "black" }}>Footer</p>
+      <Container maxWidth="xl">
+        <Grid2
+          container
+          spacing={4}
+          justifyContent="center"
+          alignItems="center"
+          columns={2}
+        >
+          <div
+            style={{
+              width: "220px",
+            }}
+          >
+            <a
+              href="https://simonyi.bme.hu/"
+              target="_blank"
+            >
+              <Image
+                src="/schonherz.svg"
+                alt="Schönherz Kollégium"
+                width={60}
+                height={60}
+                style={{
+                  width: "100%",
+                }}
+              />
+            </a>
+          </div>
+          <div
+            style={{
+              width: "60px",
+            }}
+          >
+            <a
+              href="https://vik.bme.hu/"
+              target="_blank"
+            >
+              <Image
+                src="/vik.svg"
+                alt="BME Villamosmérnöki és Informatikai Kar"
+                width={60}
+                height={60}
+                style={{
+                  width: "100%",
+                }}
+              />
+            </a>
+          </div>
+          <div
+            style={{
+              width: "220px",
+            }}
+          >
+            <a
+              href="https://simonyi.bme.hu/"
+              target="_blank"
+            >
+              <Image
+                src="/simonyi.svg"
+                alt="Simonyi Károly Szakkollégium"
+                width={60}
+                height={60}
+                style={{
+                  width: "100%",
+                }}
+              />
+            </a>
+          </div>
+        </Grid2>
+      </Container>
+      <Container maxWidth="xl">
+        <Grid2
+          container
+          spacing={4}
+          justifyContent="space-around"
+          alignItems="flex-start"
+          columns={2}
+        >
+          <div
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "1rem",
+              alignItems: "center",
+              width: "200px",
+            }}
+          >
+            <div>
+              <Typography variant="h6" align="center" color="textPrimary">
+                Kapcsolat
+              </Typography>
+            </div>
+            <div>
+              <Button
+                startIcon={<EmailIcon />}
+                href="mailto:hello+logotar@schdesign.hu"
+                target={"_blank"}
+              >
+                Hibajelentés
+              </Button>
+            </div>
+          </div>
+          <div
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "1rem",
+              alignItems: "center",
+              width: "200px",
+            }}
+          >
+            <div>
+              <Typography variant="h6" align="center" color="textPrimary">
+                Weboldalaink
+              </Typography>
+            </div>
+            <div sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "1rem",
+              alignItems: "stretch",
+            }}>
+              <Button
+                size={"medium"}
+                href="https://schdesign.hu"
+                target={"_blank"}
+                sx={{ textTransform: "none" }}
+              >
+                schdesign.hu
+              </Button>
+              <Button
+                size={"medium"}
+                href="https://roadmap.schdesign.hu"
+                target={"_blank"}
+                sx={{ textTransform: "none" }}
+              >
+                roadmap.schdesign.hu
+              </Button>
+            </div>
+          </div>
+          <div
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "1rem",
+              alignItems: "center",
+              width: "200px",
+            }}
+          >
+            <div>
+              <Typography variant="h6" align="center" color="textPrimary">
+                Közösségi oldalaink
+              </Typography>
+            </div>
+            <div sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "1rem",
+              alignItems: "flex-start",
+            }}>
+              <Button
+                size="large"
+                href="https://www.youtube.com/@schdesignbme/"
+                startIcon={<YouTubeIcon />}
+                sx={{ textTransform: "none" }}
+
+              >
+                @schdesignbme
+              </Button>
+              <Button
+                size="large"
+                href="https://www.facebook.com/schdesignbme/"
+                startIcon={<FacebookIcon />}
+                sx={{ textTransform: "none" }}
+              >
+                /schdesignbme
+              </Button>
+              <Button
+                size="large"
+                href="https://www.instagram.com/schdesign.hu/"
+                startIcon={<InstagramIcon />}
+                sx={{ textTransform: "none" }}
+              >
+                @schdesign.hu
+              </Button>
+              <Button
+                size="large"
+                href="https://github.com/simonyiszk/schdesign-logotar/"
+                startIcon={<GitHubIcon />}
+                sx={{ textTransform: "none" }}
+              >
+                /schdesign-logotar
+              </Button>
+            </div>
+          </div>
+        </Grid2>
+      </Container>
     </footer>
   );
 }
